0a3ef662ae94360dfecea142001acab90d0da063
bhavyakrk@gmail.com <bhavyaarora@TECH-BHAVYA.local>
2022-09-21 22:36:16 +0530

    Creates a cron job

---
+++ scheduler/scheduler.go
@@ -0,0 +1,42 @@
+package scheduler
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	log "github.com/BHAVYAghub/Youtube-API/logging"
+	"go.uber.org/zap"
+)
+
+// TODO : Call the SubmitToTicker(&wg, GetYTData, 1*time.Minute)
+
+// SubmitToTicker submits a func to be called to a ticker
+func SubmitToTicker(wg *sync.WaitGroup, tickerFunc func() error, period time.Duration) {
+	ticker := time.NewTicker(period)
+
+	wg.Add(1)
+	go initTicker(wg, ticker, tickerFunc)
+	wg.Wait()
+}
+
+func initTicker(wg *sync.WaitGroup, ticker *time.Ticker, tickerFunc func() error) {
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case <-ticker.C:
+			currentTime := time.Now()
+			if err := tickerFunc(); err != nil {
+				log.Error("Scheduled task via ticker failed", zap.Any("time_taken", time.Since(currentTime)), zap.Error(err))
+			}
+		case <-quit:
+			ticker.Stop()
+			wg.Done()
+			return
+		}
+	}
+}
